add23ff33788b6edca3a545cab04d0a03e5309f8
Benigno Lopes <benignofernandeslopes@gmail.com>
2021-09-26 22:07:57 -0300

    Acrescentada a mensagem desejando um bom dia

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ namespace ProjetoTesteRepositorio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia! ");
-            Console.WriteLine("Como você está?");
+            Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um Bom dia! ");
+
             Console.ReadLine();
         }
     }
