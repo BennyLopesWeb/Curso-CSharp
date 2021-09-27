a4d0e774d131d003c539471b80a72078813293a2
Benigno Lopes <benignofernandeslopes@gmail.com>
2021-09-26 22:27:56 -0300

    Acrescentada traços

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@ namespace ProjetoTesteRepositorio
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("------------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um Bom dia! ");
+
+            Console.WriteLine("------------------------------------");
 
             Console.ReadLine();
         }
